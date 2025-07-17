7c174554832261267336c73bf5f2e9c050d5ab3b
Famsp <tame2543@gmail.com>
2025-07-17 11:54:02 +0700

    CRUD

--- server/controllers/restaurant.controler.js
+++ server/controllers/restaurant.controler.js
@@ -28,4 +28,85 @@ restaurantController.create = async (req, res) => {
   });
 };
 
+restaurantController.getAll = async (req, res) => {
+  await Restaurant.findAll()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((error) => {
+      res.status(500).send({ message: error.message || "Something error" });
+    });
+};
+
+restaurantController.getById = async (req, res) => {
+  const id = req.params.id;
+  await Restaurant.findByPk(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404)({ message: "Not Found restaurant with id" + id });
+      } else {
+        res.send(data);
+      }
+    })
+    .catch((error) => {
+      res.status(500).send({ message: error.message || "Something error" });
+    });
+};
+
+restaurantController.update = async (req, res) => {
+  const id = req.params.id;
+  const { title, type, imageUrl } = req.body;
+  //validate data
+  if (!title && !type && !imageUrl) {
+    res
+      .status(400)
+      .send({ message: "title , Type and imgUrl Can't be empty!" });
+    return;
+  }
+  await Restaurant.update(
+    { title: title, type: type, imageUrl: imageUrl },
+    {
+      where: { id: id },
+    }
+  )
+    .then((num) => {
+      if (num == 1) {
+        res.send({ message: "Restaurant update succesfully!" });
+      } else {
+        res.status(400).send({
+          message:
+            "Connot update restaurant with id" +
+            id +
+            "Maybe restaurant not found",
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).send({ message: error.message || "Something error" });
+    });
+};
+
+restaurantController.deleteById = async (req, res) => {
+  const id = req.params.id;
+  if (!id) {
+    res.status(404).send({ message: "ID is missing" });
+  }
+  await Restaurant.destroy({ where: { id } })
+    .then((num) => {
+      if (num === 1) {
+        res.send({ message: "Restaurant was DELETE succesfully" });
+      } else {
+        res.status(400).send({
+          message:
+            "Connot update restaurant with id" +
+            id +
+            "Maybe restaurant not found",
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).send({ message: error.message || "Something error" });
+    });
+};
+
 export default restaurantController;
--- server/routers/restaurant.router.js
+++ server/routers/restaurant.router.js
@@ -5,4 +5,16 @@ const router = express.Router();
 // POST http://localhost:5000/api/v1/restaurant
 router.post("/", restaurantController.create);
 
+// GET http://localhost:5000/api/v1/restaurant
+router.get("/", restaurantController.getAll);
+
+// GET http://localhost:5000/api/v1/restaurant/:id
+router.get("/:id", restaurantController.getById);
+
+// GET http://localhost:5000/api/v1/restaurant/:id
+router.put("/:id", restaurantController.update);
+
+// GET http://localhost:5000/api/v1/restaurant/:id
+router.delete("/:id", restaurantController.deleteById);
+
 export default router;
