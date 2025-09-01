6d353f58957ca2608623dacfb862b6a11a99636b
FamSP <tame2543@gmail.com>
2025-09-01 14:19:39 +0700

    env change

--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+const FONTEND = process.env.FONT_END_ENV;
 import restaurantRouter from "./routers/restaurant.router.js";
 import authRouter from "./routers/auth.router.js";
 import cors from "cors";
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "127.0.0.1:5173"],
+    origin: [process.env, "127.0.0.1:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "x-access-token"],
   })
